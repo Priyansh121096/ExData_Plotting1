53d05366f4f9ed44a4b123e4b2c0b7f873281619
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2022-10-15 02:27:54 +0530

    Add relevant imports.

--- utils.R
+++ utils.R
@@ -1,3 +1,6 @@
+library(data.table)
+library(grDevices)
+
 get_data = function() {
     data = fread("exdata_data_household_power_consumption/household_power_consumption.txt", sep=';', header=TRUE, na.strings='?')
     data = data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
